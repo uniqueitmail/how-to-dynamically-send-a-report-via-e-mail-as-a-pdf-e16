6e4b9d0eac9b65dce9146e5c2018339775803d81
DevExpressExampleBot <fakeEmail>
2018-04-24 21:00:13 +0300

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.IO
 Imports System.Net.Mail
 ' ...
@@ -8,47 +7,48 @@ Imports System.Windows.Forms
 
 
 Namespace SendReportAsEMailCS
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class Form1
+        Inherits Form
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			Try
-				' Create a new report.                
-				Dim report As XtraReport1 = New XtraReport1()
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-				' Create a new memory stream and export the report into it as PDF.
-				Dim mem As MemoryStream = New MemoryStream()
-				report.ExportToPdf(mem)
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+            Try
+                ' Create a new report.                
+                Dim report As New XtraReport1()
 
-				' Create a new attachment and put the PDF report into it.
-				mem.Seek(0, System.IO.SeekOrigin.Begin)
-				Dim att As Attachment = New Attachment(mem, "TestReport.pdf", "application/pdf")
+                ' Create a new memory stream and export the report into it as PDF.
+                Dim mem As New MemoryStream()
+                report.ExportToPdf(mem)
 
-				' Create a new message and attach the PDF report to it.
-				Dim mail As MailMessage = New MailMessage()
-				mail.Attachments.Add(att)
+                ' Create a new attachment and put the PDF report into it.
+                mem.Seek(0, System.IO.SeekOrigin.Begin)
+                Dim att As New Attachment(mem, "TestReport.pdf", "application/pdf")
 
-				' Specify sender and recipient options for the e-mail message.
-				mail.From = New MailAddress("someone@somewhere.com", "Someone")
-				mail.To.Add(New MailAddress(report.ExportOptions.Email.RecipientAddress, report.ExportOptions.Email.RecipientName))
+                ' Create a new message and attach the PDF report to it.
+                Dim mail As New MailMessage()
+                mail.Attachments.Add(att)
 
-				' Specify other e-mail options.
-				mail.Subject = report.ExportOptions.Email.Subject
-				mail.Body = "This is a test e-mail message sent by an application."
+                ' Specify sender and recipient options for the e-mail message.
+                mail.From = New MailAddress("someone@somewhere.com", "Someone")
+                mail.To.Add(New MailAddress(report.ExportOptions.Email.RecipientAddress, report.ExportOptions.Email.RecipientName))
 
-				' Send the e-mail message via the specified SMTP server.
-				Dim smtp As SmtpClient = New SmtpClient("smtp.somewhere.com")
-				smtp.Send(mail)
+                ' Specify other e-mail options.
+                mail.Subject = report.ExportOptions.Email.Subject
+                mail.Body = "This is a test e-mail message sent by an application."
 
-				' Close the memory stream.
-				mem.Close()
-				mem.Flush()
-			Catch ex As Exception
-				MessageBox.Show(Me, "Error sending a report." & Constants.vbLf + ex.ToString())
-			End Try
-		End Sub
-	End Class
+                ' Send the e-mail message via the specified SMTP server.
+                Dim smtp As New SmtpClient("smtp.somewhere.com")
+                smtp.Send(mail)
+
+                ' Close the memory stream.
+                mem.Close()
+                mem.Flush()
+            Catch ex As Exception
+                MessageBox.Show(Me, "Error sending a report." & vbLf & ex.ToString())
+            End Try
+        End Sub
+    End Class
 End Namespace
